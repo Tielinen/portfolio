3f727c1a0a6e569abba58c6628f9f5f01ce94cce
Tielinen <jtielinen@live.com>
2021-09-21 12:21:21 +0700

    test js form / formsubmit

--- js/jt_scripts.js
+++ js/jt_scripts.js
@@ -3,33 +3,33 @@
 // process form
 // https://developer.mozilla.org/en-US/docs/Learn/Forms/Sending_forms_through_JavaScript#using_formdata_bound_to_a_form_element
 
-// window.addEventListener( "load", function () {
-//   function sendData() {
-//     const XHR = new XMLHttpRequest();
-//     const FD = new FormData( form );
+window.addEventListener( "load", function () {
+  function sendData() {
+    const XHR = new XMLHttpRequest();
+    const FD = new FormData( form );
 
-//     XHR.addEventListener( "load", function(event) {
-//       form.reset();
-//       form.style.display = 'none';
-//       document.querySelector('.w-form-done').style.display = 'block';
-//     } );
+    XHR.addEventListener( "load", function(event) {
+      form.reset();
+      form.style.display = 'none';
+      document.querySelector('.w-form-done').style.display = 'block';
+    } );
 
-//     XHR.addEventListener( "error", function( event ) {
-//       document.querySelector('.w-form-fail').style.display = 'block';
-//     } );
+    XHR.addEventListener( "error", function( event ) {
+      document.querySelector('.w-form-fail').style.display = 'block';
+    } );
 
-//     XHR.open( "POST", "https://mailthis.to/msg@tielinen.pro" );
-//     XHR.send( FD );
-//   }
+    XHR.open( "POST", "https://formsubmit.co/janne@tielinen.pro" );
+    XHR.send( FD );
+  }
 
-//   const form = document.getElementById( "email-form" );
+  const form = document.getElementById( "email-form" );
 
-//   form.addEventListener( "submit", function ( event ) {
-//     event.preventDefault();
+  form.addEventListener( "submit", function ( event ) {
+    event.preventDefault();
 
-//     sendData();
-//   } );
-// } );
+    sendData();
+  } );
+} );
 
 //top button
 const btn = $('#top-button');
